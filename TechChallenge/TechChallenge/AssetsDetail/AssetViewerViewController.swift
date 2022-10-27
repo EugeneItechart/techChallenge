42c9a78a324b0d8857af3565128a1a2f53a4588b
Evgeny Bessilko <yevgeni.bessilko@itechart-group.com>
2022-10-27 15:41:06 +0300

    Add draft implementation of photo viewer controller.

--- TechChallenge/TechChallenge/AssetsDetail/AssetViewerViewController.swift
+++ TechChallenge/TechChallenge/AssetsDetail/AssetViewerViewController.swift
@@ -9,9 +9,103 @@ import UIKit
 
 class AssetViewerViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  private enum Constants {
+    static let magnifyingGlassIconName = "arrow.up.left.and.down.right.magnifyingglass"
   }
 
+  private let saliencyHandler = VisionSaliencyHandler()
+  private let salientObjectsLayer = CAShapeLayer()
+
+  private let imageView: ContentModeAnimatableView = {
+    let view = ContentModeAnimatableView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.contentMode = .scaleAspectFill
+    return view
+  }()
+
+  private var salientObjectsPathTransform = CGAffineTransform.identity
+  private var nextContentMode = UIView.ContentMode.scaleAspectFit
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    configureContainerView()
+    setupSalientObjectsLayer()
+    configureNavigationItem()
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    view.setNeedsLayout()
+  }
+
+  override func viewDidLayoutSubviews() {
+    updateLayersGeometry()
+    super.viewDidLayoutSubviews()
+  }
+
+  func setupChosenImage(_ image: UIImage) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      self.imageView.image = image
+      self.findAreasOfInterest()
+    }
+  }
+
+  private func configureContainerView() {
+    view.addSubview(imageView)
+    NSLayoutConstraint.activate([
+      imageView.topAnchor.constraint(equalTo: view.topAnchor),
+      imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+    ])
+  }
+
+  private func setupSalientObjectsLayer() {
+    salientObjectsLayer.strokeColor = UIColor.red.cgColor
+    salientObjectsLayer.fillColor = nil
+    imageView.layer.addSublayer(salientObjectsLayer)
+  }
+
+  private func configureNavigationItem() {
+    let config = UIImage.SymbolConfiguration(scale: .large)
+    let buttonImage = UIImage(systemName:Constants.magnifyingGlassIconName, withConfiguration: config)
+    let button = UIBarButtonItem(image: buttonImage, style: .plain, target: self, action: #selector(rightBarButtonItemTapped))
+    navigationController?.navigationBar.tintColor = .white
+    navigationItem.rightBarButtonItem = button
+    navigationController?.isNavigationBarHidden = false
+  }
+
+  private func findAreasOfInterest() {
+    let saliencyObjects = self.saliencyHandler.findAreasOfInterest(URL(string: "")!) ?? []
+    DispatchQueue.main.async { [weak self] in
+      self?.updatePathForSaliencyLayer(with: saliencyObjects)
+    }
+  }
+
+  private func updatePathForSaliencyLayer(with rects: [CGRect]) {
+    DispatchQueue.global(qos: .userInteractive).async {
+      let path = self.saliencyHandler.createBoundingPathForSalientObjects(rects, transform: self.salientObjectsPathTransform, imageView: self.imageView)
+      DispatchQueue.main.async {
+        self.salientObjectsLayer.path = path
+      }
+    }
+  }
+
+  private func updateLayersGeometry() {
+    salientObjectsLayer.frame = view.bounds
+    let scaleT = CGAffineTransform(scaleX: salientObjectsLayer.bounds.width, y: -salientObjectsLayer.bounds.height)
+    let translateT = CGAffineTransform(translationX: 0, y: salientObjectsLayer.bounds.height)
+    salientObjectsPathTransform = scaleT.concatenating(translateT)
+  }
+
+  @objc private func rightBarButtonItemTapped() {
+    // contentMode isn't animatable property
+    let contentMode: UIView.ContentMode = imageView.contentMode == .scaleAspectFit ? .scaleAspectFill : .scaleAspectFit
+    UIView.animate(withDuration: 0.3, animations: {
+      self.imageView.contentMode = contentMode
+    }, completion: { _ in
+      self.findAreasOfInterest()
+    })
+  }
 }
---
+++ TechChallenge/TechChallenge/AssetsDetail/VisionSaliencyHandler.swift
@@ -0,0 +1,57 @@
+//
+//  VisionSaliencyHandler.swift
+//  TechChallenge
+//
+//  Created by Eugene Bessilko on 10/27/22.
+//
+
+import Vision
+import CoreVideo
+import CoreImage
+import UIKit
+
+struct VisionSaliencyHandler {
+
+  func findAreasOfInterest(_ imageUrl: URL) -> [CGRect]? {
+//    let handler = VNImageRequestHandler(data: data)
+    let handler = VNImageRequestHandler(url: imageUrl, options: [:])
+    let request = VNGenerateAttentionBasedSaliencyImageRequest { _, error in
+      if let error = error {
+        print(error)
+      }
+    }
+    request.revision = VNGenerateAttentionBasedSaliencyImageRequestRevision1
+    do {
+      try handler.perform([request])
+    } catch {
+      print(error)
+    }
+
+    // allways 1 object
+    guard let observation = request.results?.first as? VNSaliencyImageObservation else { return nil }
+    return observation.salientObjects?.compactMap({ $0.boundingBox})
+  }
+
+  func createBoundingPathForSalientObjects(_ salientObjectsBoxes: [CGRect],
+                                           transform: CGAffineTransform,
+                                           imageView: ContentModeAnimatableView) -> CGPath {
+    let path = CGMutablePath()
+    for object in salientObjectsBoxes {
+      let boundingBox = object
+      print("boundingBox: \(boundingBox)")
+//      let xCord = object.topLeft.x * 320
+//      let yCord = (1 - object.topLeft.y) * 568
+//      let width = (object.topRight.x - object.bottomLeft.x) * 320
+//      let height = (object.topLeft.y - object.bottomLeft.y) * 568
+//
+//      let newBox = CGRect(x: xCord, y: yCord, width: width, height: height)
+//      var bottomToTopTransform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -1)
+//      let rect = boundingBox.applying(bottomToTopTransform)
+      let newBox = VNImageRectForNormalizedRect(boundingBox, Int(imageView.image!.size.width), Int(imageView.image!.size.height))
+      print("newBox: \(newBox)")
+      path.addRect(boundingBox, transform: transform)
+    }
+    return path
+  }
+
+}
