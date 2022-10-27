294155fb708f54bbad938ae2fdecadf44ea87938
Evgeny Bessilko <yevgeni.bessilko@itechart-group.com>
2022-10-27 11:06:27 +0300

    Add basic setup for the project.

---
+++ TechChallenge/TechChallenge/AssetViewerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AssetViewerViewController.swift
+//  TechChallenge
+//
+//  Created by Eugene Bessilko on 10/27/22.
+//
+
+import UIKit
+
+class AssetViewerViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
---
+++ TechChallenge/TechChallenge/AssetsListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsListViewController.swift
+//  TechChallenge
+//
+//  Created by Eugene Bessilko on 10/27/22.
+//
+
+import UIKit
+
+class AssetsListViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
